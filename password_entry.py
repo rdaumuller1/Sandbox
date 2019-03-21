84bb01d0afb89c4588e6cc136222b79d88cc4a7f
jc427738 <renee.daumuller@my.jcu.edu.au>
2019-03-21 13:28:12 +1000

    Insert hand written code

--- password_entry.py
+++ password_entry.py
@@ -1,2 +1,12 @@
 """Renee Daumuller"""
 
+MIN_LENGTH = 3
+
+password = input("Password: ")
+
+while MIN_LENGTH >= 0:
+    if len(password) >= MIN_LENGTH:
+        print(len(password) * '*')
+    password = input("Password: ")
+
+
